2c0219ca313cf6117064106248f7908066a49cbd
pc <pc@user>
2022-08-29 12:08:30 +0530

    script

---
+++ src/test/java/com/jenkin_practice1/classnew.java
@@ -0,0 +1,10 @@
+package com.jenkin_practice1;
+
+public class classnew {
+	public static void main(String[] args) {
+		System.out.println("guru");
+	}
+
+
+
+}
---
+++ src/test/java/com/jenkin_practice1/newclass.java
@@ -0,0 +1,8 @@
+package com.jenkin_practice1;
+
+public class newclass {
+	public static void main(String[] args) {
+		System.out.println("raju");
+	}
+
+}
